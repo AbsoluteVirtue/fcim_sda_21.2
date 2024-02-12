d50bd20bc8ac9f0ee85e1e6229fa986417b5f80d
AbsoluteVirtue <3hitemouse@gmail.com>
2024-02-12 22:32:42 +0200

    fixed variants

--- lw1.c
+++ lw1.c
@@ -10,8 +10,6 @@ int val_in_range(int lower, int upper)
 
 int *cons(size_t n);
 size_t size(void *);
-void insert(void *a, size_t i, int v);
-void erase(void *a, size_t i);
 void clear(void *);
 bool empty(void *);
 bool eq(void *a, void *b);
@@ -19,7 +17,8 @@ bool eq(void *a, void *b);
 // vector
 void push_back(void *, int);
 size_t get(int);
-void erase_if(void *a, int v, bool (*p)(int));
+void insert(void *a, size_t i, int v);
+void erase(void *a, size_t i);
 // stack
 void pop(void *);
 void push(void *, int);
@@ -27,27 +26,29 @@ int top(void *);
 // double-ended queue
 void push_front(void *, int);
 void pop_front(void *, int);
-void erase_if(void *a, int v, bool (*p)(int));
+void insert(void *a, size_t i, int v);
+void erase(void *a, size_t i);
 // queue
 int front(void *);
 int back(void *);
 void pop(void *);
 void push(void *, int);
+// bonus
+void erase_if(void *a, int v, bool (*p)(int));
 
 int main( void ) {
 
     size_t n = val_in_range(1, 100);
+
     int *a = cons(n);
 
-    for (size_t i = 0; i < n; i++)
-    {
+    for (size_t i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
     
     insert(a, val_in_range(0, n - 1), 0);
 
-    for (size_t i = 0; i < size(a); i++)
-    {
+    for (size_t i = 0; i < size(a); i++) {
         printf("%d ", a[i]);
     }
 
@@ -55,8 +56,9 @@ int main( void ) {
 
     clear(a);
 
-    for (size_t i = 0; i < size(a); i++)
-    {
+    for (size_t i = 0; i < size(a); i++) {
         printf("%d ", a[i]);
     }
+
+    assert(empty(a));
 }
