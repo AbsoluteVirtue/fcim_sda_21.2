f79d455ca9b9f077f8bc83f5a9379f013881741c
AbsoluteVirtue <3hitemouse@gmail.com>
2024-02-29 15:57:12 +0200

    added set I

---
+++ lw_set.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int val_in_range(int lower, int upper) {
+    return lower + (rand() % (upper - lower));
+}
+
+void   clear(void *);
+bool   empty(void *);
+bool   contains(void *, int);
+void * find(void *, int);
+size_t size(void *);
+size_t count(void *, int);
+void   insert(void *, int);
+void   erase(void *, int);
+
+int main( void ) {
+    void *a;
+
+    insert(&a, 1);
+    insert(&a, 1);
+    insert(&a, 1);
+
+    assert(contains(a, 1));
+    assert(1 == count(a, 1));
+    assert(NULL == find(a, 0));
+
+    erase(&a, 1);
+
+    assert(empty(a));
+    assert(0 == size(a));
+
+    clear(&a);
+
+}
