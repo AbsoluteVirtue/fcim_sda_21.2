16c0aae4e38ff3338174934997cb1a17b5c9b77a
AbsoluteVirtue <3hitemouse@gmail.com>
2024-02-29 12:37:07 +0200

    added new files

---
+++ dev_pair.c
@@ -0,0 +1,19 @@
+// godbolt.org/z/Gd4j6nnv3
+
+// compiles on GCC trunk with -std=c2x
+
+// see: https://www.open-std.org/jtc1/sc22/wg14/www/docs/n3007.htm
+// see: https://www.open-std.org/jtc1/sc22/wg14/www/docs/n3037.pdf
+
+#define pair(T, U) struct pair_##T##_##U { T first; U second; }
+
+pair(int, float) do_thing(int a, bool b) {
+    return (pair(int, float)){a * 2, b ? 1.0f : 2.0f };
+}
+
+#include <stdio.h>
+
+int main () {
+    auto result = do_thing(50, false);
+    printf("%zu-byte sized struct: { %d, %f }", sizeof(result), result.first, result.second); 
+}
